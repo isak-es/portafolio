15421ed5d24a788350771ba64d17c9eab530e0d9
Isaac Escobar <isak.esm@gmail.com>
2026-08-19 16:46:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// --- BLOQUE SOBRE MÍ ---
+const sobreMi = document.getElementById('sobreMi');
+const infoExtra = document.getElementById('infoExtra');
+
+sobreMi.addEventListener('mouseenter', () => {
+  infoExtra.classList.remove('d-none');
+  infoExtra.style.opacity = 1;
+});
+
+sobreMi.addEventListener('mouseleave', () => {
+  infoExtra.classList.add('d-none');
+  infoExtra.style.opacity = 0;
+});
+
+// --- BLOQUE CONTACTO ---
+const contactoWrapper = document.getElementById('contactoWrapper');
+const infoContacto = document.getElementById('infoContacto');
+
+contactoWrapper.addEventListener('mouseenter', () => {
+  infoContacto.classList.remove('d-none');
+  infoContacto.style.opacity = 1;
+});
+
+contactoWrapper.addEventListener('mouseleave', () => {
+  infoContacto.classList.add('d-none');
+  infoContacto.style.opacity = 0;
+});
+
+// --- BLOQUE PORTAFOLIO ---
+const portafolioWrapper = document.getElementById('portafolioWrapper');
+const portafolioLinks = document.getElementById('portafolioLinks');
+
+portafolioWrapper.addEventListener('mouseenter', () => {
+  portafolioLinks.classList.remove('d-none');
+  portafolioLinks.style.opacity = 1;
+});
+
+portafolioWrapper.addEventListener('mouseleave', () => {
+  portafolioLinks.classList.add('d-none');
+  portafolioLinks.style.opacity = 0;
+});
